8ef8ce8d09eb9ed4f78709edcac28c59ad965256
Lesley Chang <lesley.hanna98@gmail.com>
2021-10-25 15:10:42 -0400

    add eslint prettier configs

--- app/craco.config.js
+++ app/craco.config.js
@@ -1,7 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CracoAlias = require("craco-alias");
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        baseUrl: "./src",
+        tsConfigPath: "./tsconfig.paths.json"
+      }
+    }
+  ],
   style: {
     postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")],
-    },
-  },
+      plugins: [require("tailwindcss"), require("autoprefixer")]
+    }
+  }
 };
