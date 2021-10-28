7cb8eb20454f1d7df81ec808e46bcf930cd2be4f
Lesley Chang <lesley.hanna98@gmail.com>
2021-10-28 13:25:08 -0400

    Fix TS errors excluding Web3 context related ones

--- app/src/components/MintInputField.tsx
+++ app/src/components/MintInputField.tsx
@@ -1,6 +1,11 @@
 import { InputContainer, MintSelect } from "@components";
 
-export function MintInputField({ error, onChange }) {
+interface MintInputFieldProps {
+  error: string | null;
+  onChange: (val: string) => void;
+}
+
+export function MintInputField({ error, onChange }: MintInputFieldProps) {
   async function _onChange(e: any) {
     // TODO link this to MintSelect
     onChange(e.target.value);
